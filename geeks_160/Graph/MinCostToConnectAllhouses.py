47442664db614da80ed41825d25070cc06c4cf1e
Mayank Dhaka <may9ank1@gmail.com>
2025-04-10 23:15:14 +0530

    10April2025

---
+++ geeks_160/Graph/MinCostToConnectAllhouses.py
@@ -0,0 +1,63 @@
+'''
+Given a 2D array houses[][], consisting of n 2D coordinates {x, y} where each coordinate represents the location of each house, the task is to find the minimum cost to connect all the houses of the city.
+
+The cost of connecting two houses is the Manhattan Distance between the two points (xi, yi) and (xj, yj) i.e., |xi – xj| + |yi – yj|, where |p| denotes the absolute value of p.
+
+Examples :
+
+Input: n = 5 houses[][] = [[0, 7], [0, 9], [20, 7], [30, 7], [40, 70]]
+Output: 105
+Explanation:
+Connect house 1 (0, 7) and house 2 (0, 9) with cost = 2
+Connect house 1 (0, 7) and house 3 (20, 7) with cost = 20
+Connect house 3 (20, 7) with house 4 (30, 7) with cost = 10 
+At last, connect house 4 (30, 7) with house 5 (40, 70) with cost 73.
+All the houses are connected now.
+The overall minimum cost is 2 + 10 + 20 + 73 = 105.
+
+Input: n = 4 houses[][] = [[0, 0], [1, 1], [1, 3], [3, 0]]
+Output: 7
+Explanation: 
+Connect house 1 (0, 0) with house 2 (1, 1) with cost = 2
+Connect house 2 (1, 1) with house 3 (1, 3) with cost = 2 
+Connect house 1 (0, 0) with house 4 (3, 0) with cost = 3 
+The overall minimum cost is 3 + 2 + 2 = 7.
+Constraint:
+1 ≤ n ≤ 103
+0 ≤ houses[i][j] ≤ 103
+'''
+class Solution:
+    def minCost(self, houses):
+    # code here
+
+        n = len(houses)
+        graph = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                graph.append((abs(houses[i][0] - houses[j][0]) + abs(houses[i][1] - houses[j][1]), i, j))
+        graph.sort()
+        parent = [i for i in range(n)]
+        rank = [0] * n
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    parent[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    parent[rootX] = rootY
+                else:
+                    parent[rootY] = rootX
+                    rank[rootX] += 1
+        total_cost = 0
+        for cost, u, v in graph:
+            if find(u) != find(v):
+                union(u, v)
+                total_cost += cost
+        return total_cost
+sol = Solution()
+print(sol.minCost([[0, 7], [0, 9], [20, 7], [30, 7], [40, 70]])) # Output: 105
